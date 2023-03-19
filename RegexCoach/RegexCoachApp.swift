8b9978683c7a030e434b03b942acb394bed72c2c
Philipp <pd95@users.noreply.github.com>
2023-03-19 16:26:47 +0100

    Initial Commit

---
+++ RegexCoach/RegexCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegexCoachApp.swift
+//  RegexCoach
+//
+//  Created by Philipp on 19.03.23.
+//
+
+import SwiftUI
+
+@main
+struct RegexCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
